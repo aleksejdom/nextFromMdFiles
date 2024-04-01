04f1ce6ffa244d0c63fbbdde8a04e3c926011b30
adomovec <aleksej.domovec@xim.ag>
2024-04-01 13:01:16 +0200

    file path added

--- app/lib/postsFromFile.js
+++ app/lib/postsFromFile.js
@@ -1,9 +1,6 @@
 import { readdir, readFile } from 'node:fs/promises';
 import { marked } from 'marked';
 import matter from 'gray-matter';
-import path from 'node:path';
-
-const blogDirectory = path.join(__dirname, 'app', 'content', 'blog');
 
 
 export async function getFeaturedReview() {
@@ -12,8 +9,7 @@ export async function getFeaturedReview() {
 }
 
 export async function getPostsFromFiles(slug) {
-  const filePath = path.join(blogDirectory, `${slug}.md`);
-  const text = await readFile(filePath, 'utf8');
+  const text = await readFile(`./content/blog/${slug}.md`, 'utf8');
   const { content, data:{ title, date, image} } = matter(text);
   const body = marked(content, { headerIds: false, mangle: false });
   return { slug, title, date, image, body };
@@ -31,7 +27,7 @@ export async function getPosts() {
 }
 
 export async function getSlugs() {
-  const files = await readdir(blogDirectory);
+  const files = await readdir('./content/blog');
   return files.filter((file)=>file.endsWith('.md'))
     .map((file)=>file.slice(0,-'.md'.length));
 }
